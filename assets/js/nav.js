fe1d29a0b57e08a8856d370d20bf041472e1349f
Juan de Dios López Gálvez <juandedioslopgal@gmail.com>
2022-01-15 22:25:30 -0600

    Revert "Avances"
    This reverts commit a6713c3e370e55e7762af112a0e8ebd67fbbfe70.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -4,18 +4,19 @@ let createNav = () =>{
     <div class="nav">
         <a href="index.html">Ventas <span>Plus</span></a>
         <div class="nav-items">
-            <div class="search" >
-                <input id="search" type="text" class="search-box" placeholder="Buscador de productos">
-                <button class="search-btn" id="search-btn">Buscar</button>
+            <div class="search">
+                <input type="text" class="search-box" placeholder="Buscador de productos">
+                <button class="search-btn">Buscar</button>
             </div>
             <a href="#"><i class="fas fa-user"></i></a>
             <a href="#"><i class="fas fa-shopping-cart"></i></a> 
         </div>
     </div>
     <ul class="links-container">
-        <li class="link-item"> <a href="index.html" class="link">Inicio</a></li>
+        <li class="link-item"> <a href="#" class="link">Inicio</a></li>
         <li class="link-item"> <a href="#" class="link">Hombre</a></li>
         <li class="link-item"> <a href="#" class="link">Mujer</a></li>
+        <li class="link-item"> <a href="#" class="link">Niños</a></li>
         <li class="link-item"> <a href="#" class="link">Accesorios</a></li>
     </ul>
     `;
